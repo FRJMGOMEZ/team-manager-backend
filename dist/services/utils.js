dab2e2a2b19695499d34ee84bcefbf1075e51547
frjmgomez <36133903+FRJMGOMEZ@users.noreply.github.com>
2021-02-28 19:00:05 +0100

    Delete dist directory

--- dist/services/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.inMongoId = void 0;
-const ObjectId = require('mongoose').Types.ObjectId;
-exports.inMongoId = (id) => {
-    return ObjectId.isValid(id);
-};
