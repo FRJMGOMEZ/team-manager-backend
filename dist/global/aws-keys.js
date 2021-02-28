ac5e80e973b577b45ecab5d0bb048a8d3549e24b
Francisco Javier Martinez <frjmartinezgomez@gmail.com>
2021-02-28 18:34:00 +0100

    +actions required system

--- dist/global/aws-keys.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-process.env.AWS_S3_BUCKET_NAME = 'team-manager90000';
-process.env.AWS_ACCESS_KEY_ID = "AKIAQ2AXEBQ4FJOUHY4E";
-process.env.AWS_SECRET_ACCESS_KEY = "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";
